21db154c2ba8ecd22627575e7b38859d3315027b
williford-abunas <abunaswilliford@gmail.com>
2026-03-08 23:12:11 +1300

    initial commit

--- 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,28 @@
+import {data} from '../../../data.js';
+import { useState } from 'react';
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  }
+
+  const clearList = () => {
+    setPeople([]);
+  }
+
+  return (
+    <><ul>
+      {people.map((person) => {
+        return <><li key={person.id}>{person.name}<button onClick={() => removeItem(person.id)}>Remove</button></li></>;
+
+      })}
+    </ul><button onClick={clearList}>Clear List</button></>
+  );
+
+   
 };
 
 export default UseStateArray;
