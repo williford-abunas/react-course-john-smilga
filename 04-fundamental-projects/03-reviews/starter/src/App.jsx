7746a3e36ce2ab9fffffbe2fbefbd6e4885b9ea8
williford-abunas <abunaswilliford@gmail.com>
2026-03-15 23:16:19 +1300

    reviews project done

--- 04-fundamental-projects/03-reviews/starter/src/App.jsx
+++ 04-fundamental-projects/03-reviews/starter/src/App.jsx
@@ -1,4 +1,62 @@
+import { useState, useEffect } from "react";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+import Button from "./Button";
+import people from "./data";
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  }
+
+  const nextPerson = () => {
+    setIndex(checkNumber(index + 1));
+  }
+
+  const prevPerson = () => {
+    setIndex(checkNumber(index - 1));
+  }     
+  
+
+  const randomPerson = () => {
+    let randomIndex = Math.floor(Math.random() * people.length);
+    if (randomIndex === index) {
+      randomIndex = index + 1;
+    }
+    setIndex(checkNumber(randomIndex));
+  }
+
+  return <main>
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img"/>
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4>{name}</h4>
+      <p className="job">{job}</p>
+      <p className="text">{text}</p>
+      <div className="button-container">
+        <Button onClick={prevPerson} className="prev-btn">
+          <FaChevronLeft />
+        </Button>
+        <Button onClick={nextPerson} className="next-btn">
+          <FaChevronRight />
+        </Button>
+      </div>
+      <Button onClick={randomPerson} className="btn btn-hipster">
+        surprise me
+      </Button>
+    </article>
+  </main>;
 };
 export default App;
---
+++ 04-fundamental-projects/03-reviews/starter/src/Button.jsx
@@ -0,0 +1,11 @@
+
+
+const Button = ({ children, ...props }) => {
+  return (
+    <button {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
