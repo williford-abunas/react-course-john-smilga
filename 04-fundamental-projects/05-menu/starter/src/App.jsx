e28f7d914557938940e204f14b567a0106e79ff5
williford-abunas <abunaswilliford@gmail.com>
2026-03-16 23:11:01 +1300

    Menus projectt done

--- 04-fundamental-projects/05-menu/starter/src/App.jsx
+++ 04-fundamental-projects/05-menu/starter/src/App.jsx
@@ -1,4 +1,29 @@
+import menu from './data';
+import { useState } from 'react';
+import Title from "./components/Title";
+import Menu from "./components/Menu";
+import Categories from './components/Categories';
+
+const allCategories = ['all', ...new Set(menu.map((item) => item.category))];
+
 const App = () => {
-  return <h2>Menu Starter</h2>;
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+  return <main>
+    <section className="menu">
+      <Title text='our menu'/>
+      <Categories categories={categories} filterItems={filterItems}/>
+      <Menu items={menuItems}/>
+    </section>
+  </main>
 };
 export default App;
---
+++ 04-fundamental-projects/05-menu/starter/src/components/Categories.jsx
@@ -0,0 +1,21 @@
+const Categories = ({ categories, filterItems }) => {
+  
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button
+            type="button"
+            className="btn"
+            key={index}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
