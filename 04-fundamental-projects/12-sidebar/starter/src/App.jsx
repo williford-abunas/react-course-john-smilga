de06eb752dcac0a9ff3fe7b22a641c09fcadf3ef
williford-abunas <abunaswilliford@gmail.com>
2026-09-07 20:50:37 +1200

    finish sidebar project

--- 04-fundamental-projects/12-sidebar/starter/src/App.jsx
+++ 04-fundamental-projects/12-sidebar/starter/src/App.jsx
@@ -1,4 +1,13 @@
+import Home from "./components/Home";
+import Modal from "./components/Modal";
+import Sidebar from "./components/Sidebar";
+
 const App = () => {
-  return <h2>Sidebar Starter</h2>;
+  return <>
+    <Home />
+    <Modal />
+    <Sidebar />
+  </>
+  ;
 };
 export default App;
